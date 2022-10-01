5798404202e354695c7038b19a2e32accdb5fb52
bottler3123 <109344835+bottler3123@users.noreply.github.com>
2022-10-01 23:56:39 +0900

    Add files via upload

--- Main_12851_숨바꼭질2.java
+++ Main_12851_숨바꼭질2.java
@@ -1,4 +1,4 @@
-package algorithm;
+package practice;
 
 import java.util.LinkedList;
 import java.util.Queue;
@@ -14,8 +14,9 @@ public class Main_12851_숨바꼭질2 {
 		Queue<int[]> que = new LinkedList<>();
 		int cnt = 0;
 		int small = 0;
-		int routeCnt = 0;
+		int [] routeCnt = new int [100001];
 		que.offer(new int [] {N,cnt});
+		map[N] = 1;
 		
 		
 		while (!que.isEmpty()) {
@@ -24,28 +25,24 @@ public class Main_12851_숨바꼭질2 {
 			cnt = temp[1];
 					
 			if (N == K) {
-				if (small == 0) {
-					small = cnt;
-				}
-				if (small == cnt) {
-					routeCnt++;
-				}
-				continue;
+				small = map[N];
+				routeCnt++;
+				return;
 			}
 			if (N > 0 && N < 100000) {
-				if (!map[N+1]) {
+				if (map[N+1] == 0) {
 					que.offer(new int [] {N+1,cnt+1});
-					map[N+1] = true;
+					map[N+1] = map[N] + 1;
 				}
-				if (!map[N-1]) {
+				if (map[N-1] == 0) {
 					que.offer(new int [] {N-1,cnt+1});
-					map[N-1] = true;
+					map[N-1] = map[N] + 1;
 				}				
 			}
 			if (N < 5001 && N != 0) {
-				if (!map[N*2]) {
+				if (map[N*2] == 0) {
 					que.offer(new int [] {N*2,cnt+1});
-					map[N*2] = true;
+					map[N*2] = map[N] + 1;
 				}				
 			}
 		}
