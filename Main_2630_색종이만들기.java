cab70d17e97e2fbab8f2cd58eb6cc42d63264c67
bottler3123 <109344835+bottler3123@users.noreply.github.com>
2022-10-21 16:59:01 +0900

    Add files via upload

--- Main_2630_색종이만들기.java
+++ Main_2630_색종이만들기.java
@@ -1,4 +1,4 @@
-package solvedac_class3;
+package practice;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,48 +7,57 @@ import java.util.StringTokenizer;
 
 public class Main_2630_색종이만들기 {
 
-	static int[][] map;
-	static int blue, white, N;
-
+	static int [][] map;
+	static int blue, white;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(in.readLine());
-		map = new int[N][N];
+		int N = Integer.parseInt(in.readLine());
+		map = new int [N][N];
 		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+			StringTokenizer st = new StringTokenizer(in.readLine()," ");
 			for (int j = 0; j < N; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-
-		cuting(0, 0, N);
-		System.out.println(white);
+		
+		cuting(0,0,N);
 		System.out.println(blue);
+		System.out.println(white);
+	}
+	
+	private static void cuting(int x, int y, int N) {
+		
+		boolean check = checking(x,y,N);
+		
+		if (check) {
+			if (map[x][y] == 1) {
+				white++;
+			} else {
+				blue++;
+			}
+		} else {
+			cuting(x,y,N/2);
+			cuting(x+N/2,y,N/2);
+			cuting(x,y+N/2,N/2);
+			cuting(x+N/2,y+N/2,N/2);
+		}
+		
 	}
 
-	private static void cuting(int x, int y, int N) {
-
+	private static boolean checking(int x, int y, int N) {
+		
 		int color = map[x][y];
-		boolean check = true;
-		for (int i = x; i < N; i++) {
-			for (int j = y; j < N; j++) {
-				if (map[i][j] != color)
-					check = false;
-			}
+		
+		for (int i = x; i < x+N; i++) {
+			for (int j = y; j < y+N; j++) {
+				if (map[i][j] == color) {
+					continue;
+				} else {
+					return false;
+				}
+			}	
 		}
-
-		if (check) {
-			if (color == 1) {
-				blue++;
-			} else {
-				white++;
-			}
-			return;
-		}
-		cuting(x, y, N / 2);
-		cuting(x, y + N / 2, N / 2);
-		cuting(x + N / 2, y, N / 2);
-		cuting(x + N / 2, y + N / 2, N / 2);
-
+		
+		return true;
 	}
 }
